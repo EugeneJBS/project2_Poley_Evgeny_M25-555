2fa4e79b06bbf1f34c518ee9231325fa110fc3e1
EugeneJBS <evgenypoley@gmail.com>
2026-02-15 22:11:25 +0300

    Initial commit: corrections

--- src/primitive_db/main.py
+++ src/primitive_db/main.py
@@ -1,7 +1,14 @@
-from .engine import run, welcome
+#!/usr/bin/env python3
+"""
+Точка входа в приложение Primitive DB.
+"""
+
+from src.primitive_db.engine import run, welcome
 
 
 def main() -> None:
+    """Запуск приветствия и основного цикла программы."""
+    print("Проект запущен!")
     welcome()
     run()
 
